9b2ef9ac0ab28bbf59c062cb271560aae86f923c
kev24 <kivito2009@hotmail.com>
2021-03-04 18:04:52 -0500

    nada en los polyfills

--- detector-cara/src/polyfills.ts
+++ detector-cara/src/polyfills.ts
@@ -63,14 +63,3 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
-// import '@tensorflow/tfjs-node';
-
-// // implements nodejs wrappers for HTMLCanvasElement, HTMLImageElement, ImageData
-// import * as canvas from 'canvas';
-
-import * as faceapi from 'face-api.js';
-
-// // patch nodejs environment, we need to provide an implementation of
-// // HTMLCanvasElement and HTMLImageElement
-// const { Canvas, Image, ImageData } = canvas
-// faceapi.env.monkeyPatch({ Canvas, Image, ImageData })
