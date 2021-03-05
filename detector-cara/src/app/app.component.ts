f7a976e147f7c857f359a1c5629c254d364569a1
kev24 <kivito2009@hotmail.com>
2021-03-04 19:11:53 -0500

    puntos en cara

--- detector-cara/src/app/app.component.ts
+++ detector-cara/src/app/app.component.ts
@@ -1,22 +1,7 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import * as faceapi from 'face-api.js';
 import * as canvas from 'canvas';
-import { TNetInput } from 'face-api.js';
-import * as fs from 'fs';
 import { faceDetectionOptions } from './comatntes/detectionOpcion';
-
-const { Canvas, Image, ImageData } = canvas;
-
-console.log(typeof Canvas, canvas);
-const faceDetectionNet = faceapi.nets.faceExpressionNet;
-// export const faceDetectionNet = tinyFaceDetector
-
-// SsdMobilenetv1Options
-const minConfidence = 0.5;
-
-// TinyFaceDetectorOptions
-const inputSize = 408;
-const scoreThreshold = 0.5;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -24,16 +9,26 @@ const scoreThreshold = 0.5;
 })
 export class AppComponent implements OnInit {
   title = 'detector-cara';
-  @ViewChild('canvas', { static: true }) 
-  canvas2: ElementRef<HTMLCanvasElement>;  
+
+  @ViewChild('canvas1', { static: true })
+  canvas2: ElementRef<HTMLCanvasElement>;
   private ctx: CanvasRenderingContext2D;
-  // imagen = new Image();
+
+  @ViewChild('video')
+  public video: ElementRef;
+
+  @ViewChild('canvas')
+  public canvas: ElementRef;
+
+  public captures: Array<any>;
+
+  public constructor() {
+    this.captures = [];
+  }
 
   async ngOnInit() {
     this.ctx = this.canvas2.nativeElement.getContext('2d');
-    // faceapi.env.monkeyPatch({
-    //   readFile: () => fs.readFile('../assets/ssd_mobilenetv1_model-shard1')
-    // });
+    // cargando los modelos
     await faceapi.loadSsdMobilenetv1Model('../assets/weights');
     await faceapi.loadTinyFaceDetectorModel('../assets/weights');
     await faceapi.loadMtcnnModel('../assets/weights');
@@ -41,36 +36,66 @@ export class AppComponent implements OnInit {
     await faceapi.loadFaceLandmarkTinyModel('../assets/weights');
     await faceapi.loadFaceRecognitionModel('../assets/weights');
     await faceapi.loadFaceExpressionModel('../assets/weights');
-    console.log(await faceapi.nets.faceLandmark68Net.isLoaded);
-    console.log(await faceapi.nets.faceExpressionNet.isLoaded);
-    const img: any = await canvas.loadImage('../assets/images/surprised.jpg'); //../images/surprised.jpg
-    // this.imagen.src = "../../../../wwwroot/dist/assets/blackBoards/NCAA_mhalfcourt_500x410.png";
-    // this.imagen.onload = ()=> {
-      let imga:any = document.getElementById("myImg");
-      this.ctx.drawImage(img, 0, 0, img.width, img.height);
-  //}  
-    
-    const results = await faceapi.detectAllFaces(img, faceDetectionOptions)
-    .withFaceLandmarks()
-    .withFaceExpressions();
+    // renderizar imagen en canvas
+    const img: any = await canvas.loadImage('../assets/images/surprised.jpg');
+    this.ctx.drawImage(img, 0, 0, img.width, img.height);
+    // deteccion de imagen
+    const results = await faceapi
+      .detectAllFaces(img, faceDetectionOptions)
+      .withFaceLandmarks()
+      .withFaceExpressions();
     console.log('results');
     console.log(results);
+    const landmarks1 = await faceapi.detectFaceLandmarks(img);
+    const landmarks2 = await faceapi.detectFaceLandmarksTiny(img);
+    const descriptor = await faceapi.computeFaceDescriptor(img);
 
-    const detectionsForSize = faceapi.resizeResults(results, {
+
+    console.log(landmarks1);
+    console.log(landmarks2);
+    console.log(descriptor);
+
+    const detectionsWithLandmarks = await faceapi
+  .detectAllFaces(img)
+  .withFaceLandmarks()
+// resize the detected boxes and landmarks in case your displayed image has a different size than the original
+
+
+    // dibujar cuadrado en decteccion de imagen
+    // const detectionsForSize = faceapi.resizeResults(results, {
+    //   width: img.width,
+    //   height: img.height,
+    // });
+    const c = this.ctx.canvas;
+    c.style.width = img.width;
+    c.style.height = img.height;
+    const resizedResults = faceapi.resizeResults(detectionsWithLandmarks,  {
       width: img.width,
       height: img.height,
-    });        
-    const c = this.ctx.canvas
-    c.style.width = img.width;
-    c.style.height = img.height;            
-    var image = document.getElementById('myImg');
-    faceapi.draw.drawDetections(c, detectionsForSize);
-    this.ctx.moveTo(100,100);
+    })
+// draw detections into the canvas
+faceapi.draw.drawDetections(c, resizedResults)
+// draw the landmarks into the canvas
+faceapi.draw.drawFaceLandmarks(c, resizedResults)
+    // faceapi.draw.drawDetections(c, detectionsForSize);
   }
 
-  faceDetectionOptions(net: faceapi.NeuralNetwork<any>) {
-    return net === faceapi.nets.faceExpressionNet
-      ? new faceapi.SsdMobilenetv1Options({ minConfidence })
-      : new faceapi.TinyFaceDetectorOptions({ inputSize, scoreThreshold });
+  public ngAfterViewInit() {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+        console.log(stream);
+
+        var video = this.video.nativeElement;
+        video.srcObject = stream;
+        this.video.nativeElement.play();
+      });
+    }
+  }
+
+  public capture() {
+    var context = this.canvas.nativeElement
+      .getContext('2d')
+      .drawImage(this.video.nativeElement, 0, 0, 640, 480);
+    this.captures.push(this.canvas.nativeElement.toDataURL('image/png'));
   }
 }
